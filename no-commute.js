a400c3e4f02a99c51a3eb703bc769004e4ffe897
Håvard <havard.stigen.andersen@gmail.com>
2017-06-09 22:45:50 +0200

    småplukk

--- no-commute.js
+++ no-commute.js
@@ -1,22 +1,14 @@
-// The background page is asking us to find an address on the page.
 if (window == top) {
     chrome.extension.onRequest.addListener(function () {
-        timerSomFjernerCommutes();
+        setInterval(removeCommutes, 100);
     });
 }
 
-var timerSomFjernerCommutes = function () {
-    setInterval(fjernCommutes, 100);
+
+function removeCommutes() {
+    removeActivityWithText('Commute');
 }
 
-
-var fjernCommutes = function () {
-
-    //Føkk communtes
-    jQuery('div.activity:contains(Commute)').hide()
-    //Føkk zwift
-    jQuery('div.activity:contains(Zwift)').hide()
-    //Føkk jobb
-    jQuery('div.activity:contains(Jobb)').hide()
-};
-
+function removeActivityWithText(text) {
+    jQuery('div.activity:contains(' + text + ')').hide()
+}
